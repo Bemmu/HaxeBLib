98419d821c3e3c387e15af082e1f3aa20fb5d5be
Bemmu Sepponen <manga@bemmu.com>
2015-04-15 23:18:16 +0900

    Testing deepnight-style overlaying of bright scanlines under each enlarged pixel

--- Game.hx
+++ Game.hx
@@ -16,7 +16,35 @@ class Game {
 	var blob:Blob = null;
 	var dx = 1;
 
+	function overlay(a:Float, b:Float) {
+		if (a < 0.5) {
+			return 2 * a * b;
+		} else {
+			return 1 - 2*(1 - a)*(1 - b);
+		}
+	}
+
+	function brighten(pixel:UInt) {
+		// BGRA?
+
+		var r:Float = ((pixel >> 8) & 255)/255.0;
+		var g:Float = ((pixel >> 16) & 255)/255.0;
+		var b:Float = ((pixel >> 24) & 255)/255.0;
+
+		var o:Float = 1.0;
+
+		var n:Int = (pixel & 255) 
+			+ (Std.int(overlay(r, o) * 255.0) << 8)
+			+ (Std.int(overlay(g, o) * 255.0) << 16)
+			+ (Std.int(overlay(b, o) * 255.0) << 24);
+
+		return n;
+	}
+
 	function refresh(e:flash.events.Event) {
+
+		buffer.fillRect(new Rectangle(0, 0, 960, 600), 0xffff0000);
+
 		frames++;
 		if (Date.now().getTime() - fpsCountStart > 1000) {
 			if (fpsCountStart > 0) {
@@ -40,6 +68,124 @@ class Game {
 			blob.x = 100;
 			blob.anim("walk_right");
 		}
+
+/*		var mem = buffer.getPixels(new Rectangle(0, 0, buffer.width, buffer.height));
+		flash.Memory.select(mem);
+		var end = mem.position;
+		trace("fff");
+		var i:UInt = 0;
+		end = 10000;
+		while (i < end) {
+			i += 4;
+			flash.Memory.setI32(i, 0xffffffff);			
+		}
+		mem.position = end;
+*/
+//		var bytes:ByteArray = buffer.getPixels(new Rectangle(0,0,10,10));
+//		trace("argh");
+/*
+		try {
+			bytes[10] = 0xffff0000;
+			buffer.setPixels(new Rectangle(0,0,10,10), bytes);
+		} catch (foo : String) {
+			var y = 10;
+			trace("arghb");
+		}
+		trace("argha");*/
+
+		buffer.setPixel32(100, 100, 0xffffffff);
+
+		var rect = buffer.rect;
+		var size:Int = buffer.width * buffer.height * 4;
+//		var pixels:ByteArray = new ByteArray();
+//		pixels.length = size;
+		var pixels = buffer.getPixels(buffer.rect);
+		flash.Memory.select(pixels);
+
+		// Double width
+		var i:Int = size - buffer.width * 4;
+		var halfW = buffer.width / 2;
+
+		var j:Int;
+
+		i = size;
+		j = size >> 1;
+		while (i > 0) {
+			j -= 4 * buffer.width;
+			i -= 8 * buffer.width;
+
+			var xb = buffer.width * 4;
+			var xs = buffer.width * 2;
+			var pixel:UInt;
+			while (xb > 0) {
+				xb -= 8;
+				xs -= 4;
+				pixel = flash.Memory.getI32(j + xs);
+
+				flash.Memory.setI32(i + xb, pixel);
+				flash.Memory.setI32(i + xb + 4, pixel);
+
+				var brighterPixel = brighten(pixel);
+
+				flash.Memory.setI32(i + xb + buffer.width * 4, brighterPixel);
+				flash.Memory.setI32(i + xb + buffer.width * 4 + 4, brighterPixel);
+			}
+
+
+//			flash.Memory.setI32(j, 0xffff00ff);
+		}
+
+/*		while (i < size) {
+			flash.Memory.setI32(i, 0xffff00ff);
+			i += 4;
+		}
+*/
+
+/*		var smallXPos:Int;
+		var bigXPos:Int;
+		var pixel:UInt;
+//		trace("make it so");
+
+		var bigYPos:Int = size - buffer.width * 4;
+		var smallYPos:Int = buffer.width * buffer.height * 4 - buffer.width * 4;
+
+		while (bigYPos > 0) {
+
+			bigXPos = bigYPos + buffer.width * 4;
+			smallXPos = smallYPos + (buffer.width >> 1) * 4;
+
+			while (bigXPos > smallXPos) {
+//				flash.Memory.setI32(i + x*4, 0xffff00ff);
+				pixel = flash.Memory.getI32(smallXPos);
+				smallXPos -= 4;
+//				pixel = 0xffff00ff;
+	
+				if (bigXPos >= size) {
+					break;
+				}
+
+//				flash.Memory.setI32(bigXPos, pixel);
+				bigXPos -= 4;
+//				flash.Memory.setI32(bigXPos, pixel);
+				bigXPos -= 4;
+
+				flash.Memory.setI32(smallXPos, 0xffff00ff);
+
+			}
+
+			smallYPos -= buffer.width * 8;
+			bigYPos -= buffer.width * 8;
+		}
+		trace("ruu");
+*/
+//		flash.Memory.setI32((100 * buffer.width + 100) * 4, 0xffffffff);
+
+		pixels.position = 0;
+		buffer.setPixels(rect, pixels);
+
+//		buffer.setPixel32(100, 100, 0xffffffff);
+
+		return;
 	}
 
 	var channel:SoundChannel;
